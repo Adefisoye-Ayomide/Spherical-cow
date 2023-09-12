ec99c80233bb0dd134dab714b400fe27762204a5
Matthew <matthewayomide3@gmail.com>
2023-09-12 09:48:23 -0400

    This is the entire project, we could't figure out how to make each collaborators commit on the main repository

--- function_1.py
+++ function_1.py
@@ -1,70 +1,65 @@
-# Constants
-g = 9.8  # Gravitational acceleration (m/s^2)
-rho_air = 1.225  # Air density (kg/m^3)
+#We first set up the initial conditions and constants
+g = 9.8  
+m = 1000.0  #Mass of the cow (kg) 
+x = 0.0  #Initial x position (m)
+y = h = 100.0  #Initial y position we chose (m)
+vx = 10.0  #Initial x velocity (m/s)
+vy = 0.0  #Initial y velocity (m/s)
+constant_wind_resistance = 0.1  #we assumed this value for wind resistance constant
+dt = 0.01  #Time step size (s)
 
-# Initial conditions
-m = 1000.0  # Mass of the cow (kg)
-r = 1.0  # Radius of the cow (m)
-x = 0.0  # Initial x position (m)
-y = 100.0  # Initial y position (m)
-vx = 10.0  # Initial x velocity (m/s)
-vy = 0.0  # Initial y velocity (m/s)
-constant_wind_resistance = 0.1  # Selectable constant for wind resistance
-
-# Time settings
-dt = 0.01  # Time step size (s)
-
-# Functions
+#Function 1 which returns the total forces on the cow
 def total_force(x, y, vx, vy, constant):
-    # Calculate gravitational force
+    #Gravitational force
     fx = 0.0
     fy = -m * g
     
-    # Calculate wind resistance force
+    #Wind resistance force
     v = (vx**2 + vy**2)**0.5
-    wind_resistance = -constant * v**2 * rho_air * 3.141592653589793 * r**2
-    fx += wind_resistance * vx / v
-    fy += wind_resistance * vy / v
+    wind_resistance = -constant * v**2
+    fx += wind_resistance 
+    fy += wind_resistance 
     
     return fx, fy
 
+#Function 2 which returns the updated position and velocity of cow at time t
 def update_position_velocity(x, y, vx, vy, fx, fy, dt):
-    # Calculate acceleration
+    #Calculate acceleration
     ax = fx / m
     ay = fy / m
     
-    # Update velocity using the acceleration
+    #Update velocity using the acceleration
     new_vx = vx + ax * dt
     new_vy = vy + ay * dt
     
-    # Update position using the new velocity
+    #Update position using the new velocity
     new_x = x + new_vx * dt
     new_y = y + new_vy * dt
     
     return new_x, new_y, new_vx, new_vy
 
+#Function 3 which returns potential energy and kinetic energy of the cow
 def calculate_energy(x, y, vx, vy):
-    # Calculate potential energy
+    #Calculate potential energy
     potential_energy = m * g * y
     
-    # Calculate kinetic energy
+    #Calculate kinetic energy
     kinetic_energy = 0.5 * m * (vx**2 + vy**2)
     
-    # Total energy is the sum of potential and kinetic energy
+    #Total energy is the sum of potential and kinetic energy
     total_energy = potential_energy + kinetic_energy
     
     return potential_energy, kinetic_energy, total_energy
 
-# Simulation
 time = 0.0
 
-while y > 0.0:
+while y > 0.0: #This condition is to stop the simulation when the cow hits the ground. i.e. when y <=0
     fx, fy = total_force(x, y, vx, vy, constant_wind_resistance)
     x, y, vx, vy = update_position_velocity(x, y, vx, vy, fx, fy, dt)
     potential_energy, kinetic_energy, total_energy = calculate_energy(x, y, vx, vy)
     time += dt
 
-print("Simulation ended at time:", time)
+print("The simulation ended at time:", time)
 print("Final x position:", x)
 print("Final y position:", y)
 print("Final x velocity:", vx)
